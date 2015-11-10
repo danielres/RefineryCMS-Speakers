a30496bad3d9db9c48107668dd8bd7d8fae60746
Daniel Reszka <dannn.r@gmail.com>
2015-11-10 13:54:08 +0100

    Display full speaker name when listed in admin

--- app/controllers/admin/speakers_controller.rb
+++ app/controllers/admin/speakers_controller.rb
@@ -1,8 +1,7 @@
 module Admin
   class SpeakersController < Admin::BaseController
 
-    crudify :speaker,
-            :title_attribute => 'to_s', :xhr_paging => true
+    crudify :speaker, :xhr_paging => true
 
   end
 end
